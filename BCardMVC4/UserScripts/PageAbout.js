82fbff7e103cc1f7618a57792a2842eb941ee5e4
Alexander Zhigalov <alexandershowbiz@outlook.com>
2018-10-05 22:02:21 +0300

    Push it!)

---
+++ BCardMVC4/UserScripts/PageAbout.js
@@ -0,0 +1,38 @@
+﻿window.onload = function () {
+    document.getElementById("career").onclick = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/UserContent/Career.html", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    document.getElementById("aContent").innerHTML = xhr.responseText;
+                }
+            }
+        }
+        xhr.send();
+    }
+    document.getElementById("education").onclick = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/UserContent/Education.html", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    document.getElementById("aContent").innerHTML = xhr.responseText;
+                }
+            }
+        }
+        xhr.send();
+    }
+    document.getElementById("hobbies").onclick = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/UserContent/Hobbies.html", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    document.getElementById("aContent").innerHTML = xhr.responseText;
+                }
+            }
+        }
+        xhr.send();
+    }
+}
